44e3432f4b2d699f3104d517b954376f312c7191
Ashley <pinedaa@spu.edu>
2017-01-31 19:48:20 -0800

    final commit

--- battleship.js
+++ battleship.js
@@ -1,7 +1,7 @@
-var randomLoc = Math.floor(Math.random()* 5);
-var location1 randomLoc;
+var randomLoc = Math.floor(Math.random() * 5);
+var location1 = randomLoc;
 var location2 = location1 + 1;
-var location3 = location2 + 1;
+var location3 = location2 + 1; 
 var guess;
 var hits = 0;
 var guesses = 0;
